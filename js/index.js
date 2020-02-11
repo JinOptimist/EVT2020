af01ccc9dbba1273948fe6f5205ca1aeda05e868
JinOptimist <88jin@mail.ru>
2020-02-11 20:04:39 +0300

    'AddJsAndTemplate'

--- js/index.js
+++ js/index.js
@@ -7,4 +7,34 @@ $(document).ready(function(){
 		img.toggle();
 	});
 	
+	$(".spoiler .quest").click(function(){
+		$(this)
+			.closest('.spoiler')
+			.find('.answer')
+			.toggle();
+	});
+	
+	$('.icon.close').click(function(){
+		$(this)
+			.closest('.popup-parent')
+			.hide();
+	});
+	
+	$('.add-girl').click(function(){
+		var path = $('.girl-path').val();
+		
+		var card = $('.template .card').clone();
+		
+		card.find('.image img').attr('src', path);
+		
+		$('.center-block > .content').append(card);
+		
+	});
+
+	$('.header').click(function(){
+		$('.popup-parent').show();
+	});
 });
+
+
+
